c74607840588e1de0a37be24e8b64d95113f1a0c
NandoG1 <fernandogunawan291105@gmail.com>
2025-06-28 09:08:43 +0700

    base layout

--- components/layoutProvider/layoutProvider.tsx
+++ components/layoutProvider/layoutProvider.tsx
@@ -1,12 +1,22 @@
+"use client"
+
 import React from 'react';
+import { usePathname } from 'next/navigation';
+import Navbar from '../navbar/navbar';
+import Footer from '../footer/footer';
 
 interface LayoutProviderProps {
   children: React.ReactNode;
 }
 
 const LayoutProvider: React.FC<LayoutProviderProps> = ({ children }) => {
+    const pathName = usePathname()
   return (
-    <div>layoutProvider</div>
+    <>
+        {pathName !== "/login" && pathName !== "/signup" && <Navbar/>}
+        {children}
+        {pathName !== "/login" && pathName !== "/signup" && <Footer/>}
+    </>
   )
 }
 
