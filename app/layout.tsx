6809917a2aa879782070714e0b4049a7a9f38bc6
NandoG1 <fernandogunawan291105@gmail.com>
2025-07-02 08:46:20 +0700

    setup database

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import LayoutProvider from "@/components/layoutProvider/layoutProvider";
+import Toast from "@/utils/toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Toast/>
         <LayoutProvider>
          {children}
         </LayoutProvider>
--- utils/axiosAPI.ts
+++ utils/axiosAPI.ts
@@ -1,2 +1,9 @@
 import axios from "axios"
 
+const baseURL:string = "http://localhost:3000/api/"
+
+const AXIOS_API = axios.create({
+    baseURL
+})
+
+export default AXIOS_API
