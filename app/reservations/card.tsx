15231652d5a4e14b55909661d72d662df484f410
NandoG1 <fernandogunawan291105@gmail.com>
2025-07-02 07:36:42 +0700

    #ui reservations

--- app/reservations/card.tsx
+++ app/reservations/card.tsx
@@ -15,11 +15,15 @@ interface ReservationsProps{
   pricePerNight: number
 }
 
-function Card(hotel:ReservationsProps) {
+interface CardProps{
+  hotel: ReservationsProps
+}
+
+function Card({hotel} : CardProps) {
   return (
     <div className='w-[300px] min-h flex flex-col '>
         <Link href={`/details/${hotel.listingId}`}>
-          <Image src={hotel.image} alt='' className='rounded-xl shadow-xl' width={200} height={300} />
+          <Image src={hotel.image} alt='' className='rounded-xl shadow-xl' width={300} height={300} />
         </Link>
         <div className='p-2 mt-2 flex flex-col gap-4'>
           <span className='font-semibold text-lg'>
@@ -40,7 +44,7 @@ function Card(hotel:ReservationsProps) {
           <div>
             Total Price: ${hotel.daysDifference * hotel.pricePerNight}
           </div>
-          <button className='w-2 py-2 bg-red-500 text-white rounded-xl transition-all hover:bg-red-400'>Cancel</button>
+          <button className='w-48 py-2 bg-red-500 text-white rounded-xl transition-all hover:bg-red-400'>Cancel</button>
         </div> 
 
 
